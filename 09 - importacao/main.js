1b1ad3025fdc8eba80d53da97416618c7aa2ddaa
rafaelcloud83 <rafaelcloud83@gmail.com>
2025-08-02 12:11:42 -0300

    importação exportação

---
+++ 09 - importacao/main.js
@@ -0,0 +1,11 @@
+/*
+const funcoes = require('./funcoes-auxiliares');
+
+console.log(funcoes);
+console.log(funcoes.gets());
+funcoes.print('OK');
+*/
+
+const { gets, print } = require('./funcoes-auxiliares');
+
+print(gets());
